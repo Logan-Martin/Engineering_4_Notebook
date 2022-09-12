320f5a185a29c4221cf90ed1bc163bb55527af6c
Logan-Martin <lmartin27@charlottesvilleschools.org>
2022-09-12 10:45:49 -0400

    9/12/2022 Commit - Led 1

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -5,11 +5,18 @@ import time
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+redLED = digitalio.DigitalInOut(board.GP16)
+redLED.direction = digitalio.Direction.OUTPUT
+
 countDownNumber = 10
 countDownMessage = "Lift off in: "
 
-while countDownNumber > 1:
+while countDownNumber > 0:
     print(countDownMessage + str(countDownNumber))
-    time.sleep(1)
+    redLED.value = True
+    time.sleep(0.5)
+    redLED.value = False
     countDownNumber = countDownNumber - 1
+    time.sleep(0.5)
 print("Lift off!")
+led.value = True
